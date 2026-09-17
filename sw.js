488b32b517d318cf42260968c6ce6bd7ce1ebe0e
GuC-13 <clement.guerra@sbl.ch>
2026-09-17 10:38:12 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mathe-duell-cache-v18";
+const CACHE_NAME = "mathe-duell-cache-v19";
 const FILES_TO_CACHE = ["./", "./index.html"];
 
 self.addEventListener("install", (event) => {
